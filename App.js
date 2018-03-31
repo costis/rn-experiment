ff82dd34aaf3db410e7cc365e2fe33af6ddf5870
Costis Panagiotopoulos <costisp@gmail.com>
2018-03-31 21:51:13 +0100

    More styling for BeachDetail

--- App.js
+++ App.js
@@ -26,33 +26,33 @@ export default class App extends React.Component {
     beaches: [
       {
         id: 'b_navagio',
-        name: 'Tragaki',
-        description: 'blah blah',
-        type: 'sandy',
+        name: 'Shipwreck',
+        description: 'Illum assum in vix, ea nam falli ludus. Nec prompta contentiones eu, no his diam dicant aliquip, quaeque probatus complectitur in mei. His no dicam graece primis, vero atqui eam ex, soleat civibus est eu.',
+        type: 'sandy, shallow waters',
         facilities: ['taverns', 'public toilets'],
         image: require('./assets/img/navagio.jpg')
       },
       {
         id: 'b_smaria',
         name: 'Santa Maria',
-        description: `boo booblah blah asdfa afdf`,
-        type: 'sandy',
+        description: `Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        type: 'sandy, shallow waters',
         facilities: ['taverns', 'public toilets', 'beach bar'],
         image: require('./assets/img/santa_maria.jpg')
       },
       {
         id: 'b_golden',
         name: 'Golden Beach',
-        description: 'Ze foo bar',
-        type: 'rocky',
+        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        type: 'pebbles, shallow and then deep',
         facilities: ['Children\'s playground', 'beach bar'],
         image: require('./assets/img/golden_beach.jpg')
       },
       {
         id: 'b_pisolivadi',
         name: 'Piso Livadi',
-        description: 'Ze Piso Livadi',
-        type: 'rocky',
+        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        type: 'rocky, deep waters',
         facilities: ['Children\'s playground', 'beach bar', 'taverns'],
         image: require('./assets/img/piso_livadi.jpg')
       },
@@ -61,5 +61,13 @@ export default class App extends React.Component {
 
   render() {
     return <MyStack beaches={this.state.beaches}/>;
+    // const nav = {
+    //   state : {
+    //     params: {
+    //       beach: this.state.beaches[0]
+    //     }
+    //   }
+    // }
+    // return <BeachDetail navigation={nav}/>
   }
 }
--- src/components/BeachDetail/BeachDetail.js
+++ src/components/BeachDetail/BeachDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, Image, View} from 'react-native';
+import {Text, Image, View, StyleSheet, ScrollView} from 'react-native';
 
 export default class BeachDetail extends React.Component {
   static navigationOptions = ({navigation}) => {
@@ -14,9 +14,69 @@ export default class BeachDetail extends React.Component {
     const {params} = this.props.navigation.state;
     const beach = params.beach;
 
-    return <View>
-            <Text>{beach.id} - {beach.name}</Text>
-            <Image source={beach.image}/>
-          </View>
+    return(
+    <ScrollView>
+    <View>
+      <Text style={style.title}>{beach.name}</Text>
+
+      <Text style={style.description}>{beach.description}</Text>
+
+      <View style={style.rowInfo}>
+        <Text style={style.label}>Beach type</Text>
+        <Text style={style.val}>{beach.type}</Text>
+      </View>
+
+      <View style={style.rowInfo}>
+        <Text style={style.label}>Facilities</Text>
+        <Text style={style.val}>{beach.facilities.join(', ')}</Text>
+      </View>
+
+      <View style={style.rowInfo}>
+        <Text style={style.label}>Distance</Text>
+        <Text style={style.val}>350m</Text>
+      </View>
+
+      <Image source={beach.image}/>
+    </View>
+    </ScrollView>)
   }
 }
+
+const style = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingBottom: 5,
+    color: 'white',
+    backgroundColor: '#ff0000',
+    textShadowColor: 'gray',
+    textShadowOffset: {width: 2, height: 1}
+  },
+  description: {
+    padding: 12,
+    fontSize: 20,
+    lineHeight: 34
+
+  },
+  rowInfo: {
+    flexDirection: 'row',
+    marginTop: 1
+  },
+  label: {
+    backgroundColor: '#6380ad',
+    width: '40%',
+    padding: 12,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  val: {
+    backgroundColor: '#ADADAD',
+    width: '60%',
+    padding: 12,
+    fontSize: 20,
+    color: 'white'
+  }
+});
